7bb1aa58a59943e757035ab881ac61e223c4ebbc
tudorandronic <save_tudor@yahoo.com>
2025-02-03 14:34:34 +0200

    Added react hooks
    Added react hooks and a button.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import TodoTable from './components/TodoTable';
+import React,{useState} from 'react';
+
 function App() {
 
-  const todos = [
+  const [todos, setTodos] = useState([
     {rowNumber: 1, rowTask: "Feed Dog", rowAssigned: "User 1"},
     {rowNumber: 2, rowTask: "Water plants", rowAssigned: "User 2"},
-    {rowNumber: 1, rowTask: "Feed Pig", rowAssigned: "User 2"}
-  ];
+    {rowNumber: 3, rowTask: "Feed Pig", rowAssigned: "User 2"},
+    {rowNumber: 4, rowTask: "Charge phone", rowAssigned: "User 1"}
+  ]);
+
+  const addTodo = () => {
+    //if(todos.length > 0){
+      setTodos([...todos,{rowNumber: todos.length+1, rowTask:"Learn React", rowAssigned:"User 3"}]);
+    //}
+  }
 
   return (
     <div>
@@ -21,6 +30,9 @@ function App() {
             </div>
             <div className="card-body">
                 <TodoTable todoList={todos}/>
+                <button className='btn btn-primary' onClick = {addTodo}>
+                  Add new todo
+              </button>
             </div>
           </div>
       </div>
--- src/components/TodoTable.js
+++ src/components/TodoTable.js
@@ -4,25 +4,27 @@ function TodoTable(props){
     const todoList = props.todoList;
     return (
         <table className='table table-hover'>
-                  <thead>
-                    <tr>
-                        <th>
-                          #
-                        </th>
-                        <th>
-                          Description
-                        </th>
-                        <th>
-                          Assigned
-                        </th>  
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <TodoRowItem rowNumber={todoList[0].rowNumber} rowDescription={todoList[0].rowTask} rowAssigned={todoList[0].rowAssigned}/>
-                    <TodoRowItem rowNumber={todoList[1].rowNumber} rowDescription={todoList[1].rowTask} rowAssigned={todoList[1].rowAssigned}/>
-                    <TodoRowItem rowNumber={todoList[2].rowNumber} rowDescription={todoList[2].rowTask} rowAssigned={todoList[2].rowAssigned}/>
-                  </tbody>
-                </table>  
+          <thead>
+            <tr>
+                <th>
+                  #
+                </th>
+                <th>
+                  Description
+                </th>
+                <th>
+                  Assigned
+                </th>  
+            </tr>
+          </thead>
+          <tbody>
+            {
+              todoList.map(todo => (
+                  <TodoRowItem rowNumber={todo.rowNumber} rowDescription={todo.rowTask} rowAssigned={todo.rowAssigned}/>
+              ))
+            }
+          </tbody>
+        </table>  
     )
 }
 
